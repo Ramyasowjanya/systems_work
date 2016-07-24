01d41970351c7509330aab88fcb4dc7478c3014f
Ramyasowjanya <ramyasowjanya.s@gmail.com>
2016-07-24 22:20:19 +0530

    Update and rename 1lakhtree.c to secondday.c

--- btree_problems/secondday.c
+++ btree_problems/secondday.c
@@ -1,4 +1,4 @@
-/*#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
@@ -292,4 +292,3 @@ void main()
 	fclose(fp1);
 	fclose(fp2);
 }
-*/
